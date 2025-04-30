5b8e92803d2c18200bf8f03436c22477f7a29ab5
Sk Md Abdul Kaium <73533834+Abdulkaium007@users.noreply.github.com>
2025-04-30 13:07:48 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+//for mouse click
+
+var numOfButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numOfButton; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+    //this.style.color = "white";
+    var buttonHTML = this.innerHTML;
+    makeSound(buttonHTML);
+    buttonAni(buttonHTML);
+
+    
+});
+
+}
+
+
+//    var audio = new Audio("./sounds/tom-1.mp3");
+//audio.play();
+
+
+//for key press
+document.addEventListener("keypress", function(event){
+
+    makeSound(event.key);
+    buttonAni(event.key)
+})
+
+
+//audio function
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+                var audio = new Audio("./sounds/tom-1.mp3");
+                audio.play();
+            break;
+
+        case "a":
+                var audio = new Audio("./sounds/tom-2.mp3");
+                audio.play();
+            break;
+
+        case "s":
+                var audio = new Audio("./sounds/tom-3.mp3");
+                audio.play();
+            break; 
+
+        case "d":
+                var audio = new Audio("./sounds/tom-4.mp3");
+                audio.play();
+            break;    
+
+        case "j":
+                var audio = new Audio("./sounds/snare.mp3");
+                audio.play();
+            break;
+
+        case "k":
+                var audio = new Audio("./sounds/crash.mp3");
+                audio.play();
+            break;
+
+        case "l":
+                var audio = new Audio("./sounds/kick-bass.mp3");
+                audio.play();
+            break;
+
+        default:
+            break;
+}
+}
+
+function buttonAni(currentKey){
+
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed")}, 100);
+
+}
